212b0d46983917b044d0e5e6431af8a9fe25aff7
Aashu1308 <117470874+Aashu1308@users.noreply.github.com>
2023-03-23 22:11:26 +0530

    Update q4.js

--- q4.js
+++ q4.js
@@ -1,11 +1,30 @@
 nums = [20, 30, 53, 40, 50, 57, 60, 70, 80, 90];
+let indexes = [];
+let index;
+let count = 0;
+let len = nums.length;
 console.log("Numbers not matching pattern are:")
 for (num of nums) {
     i = num % 10;
     if (i != 0) {
         index = nums.indexOf(num);
         console.log(num);
-        nums.splice(index, 1);
+        indexes[indexes.length] = index - count;
+        count += 1;
     }
 }
-console.log("New list is:", nums);
+for (i of indexes) {
+    let found = false;
+    for (j = 0; j < len - 1; j++) {
+        if (i == j) {
+            found = true;
+        }
+        if (found) {
+            nums[j] = nums[j + 1];
+        }
+    }
+}
+console.log("New list is:")
+for (i = 0; i < (nums.length - count); i++) {
+    console.log(nums[i]);
+}
