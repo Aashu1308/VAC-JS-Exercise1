b69d9ce8ecf957e4b0b5bf989226bc6d54a9ec3b
Aashu1308 <117470874+Aashu1308@users.noreply.github.com>
2023-03-25 16:30:54 +0530

    Update q5.js

--- q5.js
+++ q5.js
@@ -1,15 +1,23 @@
 let a = [10, 30, 40, 60, 70, 80, 100, 110];
 let missing_pos = [];
+let len = a.length;
 for (i = 0; i < a.length - 1; i++) {
     if ((a[i + 1] - a[i]) > 10) {
         missing_pos[missing_pos.length] = i + 1;
     }
 }
 console.log("Missing Positions:", missing_pos);
-let count = 1;
+
+/*let count = 1;
 for (i of missing_pos) {
     var n = (i + count) * 10;
     a.splice((i + count - 1), 0, n);
     count++;
+}
+console.log("Complete array:", a); */
+
+len += missing_pos.length;
+for (i = 0; i < len; i++) {
+    a[i] = (i + 1) * 10;
 }
 console.log("Complete array:", a);
